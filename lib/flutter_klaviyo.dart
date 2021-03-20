7aa8b044184dcb1d6e82050ffea535d823b31f67
Johnder Baul <imjohnderbaul@gmail.com>
2021-03-21 03:24:52 +0800

    fix return types

--- lib/flutter_klaviyo.dart
+++ lib/flutter_klaviyo.dart
@@ -5,29 +5,29 @@ import 'package:flutter/services.dart';
 class FlutterKlaviyo {
   static const MethodChannel _channel = const MethodChannel('flutter_klaviyo');
 
-  static Future<void> setupWithPublicAPIKey({String apiKey}) {
+  static void setupWithPublicAPIKey({String apiKey}) {
     final args = <String, dynamic>{
       'apiKey': apiKey,
     };
-    return _channel.invokeMethod<void>('setupWithPublicAPIKey', args);
+    _channel.invokeMethod<void>('setupWithPublicAPIKey', args);
   }
 
-  static Future<void> initNotification() {
-    return _channel.invokeMethod<void>('initNotification');
+  static void initNotification() {
+    _channel.invokeMethod<void>('initNotification');
   }
 
   // TODO: add support for customerProperties and properties parameter
-  static Future<void> trackEvent({String eventName}) {
+  static void trackEvent({String eventName}) {
     final args = <String, dynamic>{
       'eventName': eventName,
     };
-    return _channel.invokeMethod<void>('trackEvent', args);
+    _channel.invokeMethod<void>('trackEvent', args);
   }
 
-  static Future<void> setUpUserEmail({String userEmail}) {
+  static void setUpUserEmail({String userEmail}) {
     final args = <String, dynamic>{
       'userEmail': userEmail,
     };
-    return _channel.invokeMethod<void>('setUpUserEmail', args);
+    _channel.invokeMethod<void>('setUpUserEmail', args);
   }
 }
