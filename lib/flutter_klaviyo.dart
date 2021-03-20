05d21fb5b32483e844592568d29b7292676384dd
Johnder Baul <imjohnderbaul@gmail.com>
2021-03-21 03:37:42 +0800

    Revert "fix return types"
    This reverts commit 7aa8b044184dcb1d6e82050ffea535d823b31f67.

--- lib/flutter_klaviyo.dart
+++ lib/flutter_klaviyo.dart
@@ -5,29 +5,29 @@ import 'package:flutter/services.dart';
 class FlutterKlaviyo {
   static const MethodChannel _channel = const MethodChannel('flutter_klaviyo');
 
-  static void setupWithPublicAPIKey({String apiKey}) {
+  static Future<void> setupWithPublicAPIKey({String apiKey}) {
     final args = <String, dynamic>{
       'apiKey': apiKey,
     };
-    _channel.invokeMethod<void>('setupWithPublicAPIKey', args);
+    return _channel.invokeMethod<void>('setupWithPublicAPIKey', args);
   }
 
-  static void initNotification() {
-    _channel.invokeMethod<void>('initNotification');
+  static Future<void> initNotification() {
+    return _channel.invokeMethod<void>('initNotification');
   }
 
   // TODO: add support for customerProperties and properties parameter
-  static void trackEvent({String eventName}) {
+  static Future<void> trackEvent({String eventName}) {
     final args = <String, dynamic>{
       'eventName': eventName,
     };
-    _channel.invokeMethod<void>('trackEvent', args);
+    return _channel.invokeMethod<void>('trackEvent', args);
   }
 
-  static void setUpUserEmail({String userEmail}) {
+  static Future<void> setUpUserEmail({String userEmail}) {
     final args = <String, dynamic>{
       'userEmail': userEmail,
     };
-    _channel.invokeMethod<void>('setUpUserEmail', args);
+    return _channel.invokeMethod<void>('setUpUserEmail', args);
   }
 }
